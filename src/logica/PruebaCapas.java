6cdb8483d473692c485eed212c3744209052dae2
Jefferson Alquinga <144504631+Jeffers199817@users.noreply.github.com>
2024-07-02 00:33:38 -0500

    Final de lenguaje Java GUI

---
+++ src/logica/PruebaCapas.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author JEFFERSON
+ */
+public class PruebaCapas {
+    
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+        
+    }
+    
+}
